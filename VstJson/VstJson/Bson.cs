1fb708308b6a0f0eb887bd2f2fc6e7870d140855
Hoang <alonet14@gmail.com>
2021-04-15 20:04:49 +0700

    hoang

---
+++ VstJson/VstJson/Bson.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using BsonData;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Bson;
+namespace Vst
+{
+    
+    public class Bson
+    {
+        public static void Write(FileStream stream, object value)
+        {
+            using (var bw = new Newtonsoft.Json.Bson.BsonWriter(stream))
+            {
+                var serializer = new Newtonsoft.Json.JsonSerializer();
+                serializer.Serialize(bw, value);
+            }
+        }
+        public static JObject Read(FileStream stream)
+        {
+            using (var br = new Newtonsoft.Json.Bson.BsonReader(stream))
+            {
+                var serializer = new Newtonsoft.Json.JsonSerializer();
+                return (JObject)serializer.Deserialize(br);
+            }
+        }
+        public static T Read<T>(FileStream stream)
+        {
+            var o = Read(stream);
+            return o.ToObject<T>();
+        }
+    }
+}
